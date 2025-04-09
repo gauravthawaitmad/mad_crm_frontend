5c056781d1e9760aa6f4792a4e0d84762d61ba7d
Gaurav Thawait <iamgauravthawait@gmail.com>
2025-04-09 21:00:08 +0530

    message popup removed from dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,5 @@
-// import DashboardModule from '@/modules/DashboardModule';
-// export default function Dashboard() {
-//   console.log("dashboard page loaded")
-//   return <DashboardModule />;
-// }
-
-
-
 import DashboardModule from '@/modules/DashboardModule';
-import PageInfoPopup from '@/components/CustomPopUp/PageInfoPopUp';
 export default function Dashboard() {
-  console.log("dashboard page loaded")
-  return(
-    <>
-    <PageInfoPopup message={''} heading={'Welcome to Dashboard'} />
-    <DashboardModule />;
-    </>
-
-  ) 
-    
+  return <DashboardModule />;
 }
+
